1219cad6e78ae30a2052d24f4cd35d0cab4078fc
Juan Manuel <jmtrak@gmail.com>
2025-07-19 11:27:00 -0600

    Actualización de dependencias, creación de scripts para funciones, suibida de datos

--- dependencias.R
+++ dependencias.R
@@ -1,3 +1,9 @@
-library(tidyverse)
-library(plumber)
-library(quarto)
+# dependencias.R
+library(tidyverse) # Paquete de limpieza, manipulación y visualización de datos
+library(plumber) # Generación de APIS
+library(quarto) # Construcción de Reportes
+library(httr2) # 
+library(janitor)
+library(glue)
+library(readxl)
+library(arrow)
